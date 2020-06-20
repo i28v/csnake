4ff63ec6ab1fddfdd9a291a964a81bd78d687352
i28v <legomail90520@gmail.com>
2020-06-20 16:11:02 -0700

    Update Game.c

--- src/Game.c
+++ src/Game.c
@@ -3,7 +3,7 @@
 char map1[32][128] = 
 {
 		"###############################################################################################################################",
-	    "#                                                                                                                             #",
+	        "#                                                                                                                             #",
 		"#                                                                                                                             #",
 		"#                                                                                                                             #",
 		"#                                                                                                                             #",
@@ -35,7 +35,6 @@ char map1[32][128] =
 		"#                                                                                                                             #",
 		"###############################################################################################################################"
 };
-
 
 void gameInitGame(struct Game* game)
 {
@@ -130,7 +129,7 @@ void gameDraw(struct Game* game)
 
 void gameMainGameLoop(struct Game* game)
 {
-    gameCheckInput(game);
+    	gameCheckInput(game);
 	gameUpdateGame(game);
 	gameDraw(game);
 }
